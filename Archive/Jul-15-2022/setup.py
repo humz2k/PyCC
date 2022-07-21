2be85ee29e8a86d03d4e6f3d6c97f453db101962
Humza Qureshi <30800959+humz2k@users.noreply.github.com>
2022-07-20 20:07:03 -0500

    remove archive

--- Archive/Jul-15-2022/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-import numpy as np
-
-setup(
-    ext_modules = cythonize("treecode.pyx"),
-    compiler_directives={'language_level' : "3"}
-)
--- Archive/experimental/run.py
+++
@@ -1,4 +0,0 @@
-import test_speed
-import time
-
-test_speed.test(1000,10)
